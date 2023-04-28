d26819b0e44a2c32f36aabcbd2c151c4f79081e0
Akram13486 <114947203+Akram13486@users.noreply.github.com>
2023-04-28 11:22:41 +0530

    Update Carbon NanoTubes.py

--- Carbon NanoTubes.py
+++ Carbon NanoTubes.py
@@ -14,18 +14,24 @@ print(data.describe())
 X = data["Calculated atomic coordinates w'"]
 Y = data.drop(columns = "Calculated atomic coordinates w'" )
 xtrain,xtest,ytrain,ytest = train_test_split(X,Y,test_size = 0.3)
+
+#linear Regression Model
 lr  = LinearRegression()
 lr.fit(xtrain.values.reshape(-1,1),ytrain)
 lr_pred = lr.predict(xtest.values.reshape(-1,1))
 lr# print(r2_score(ytest,lr_pred))
 lr_accuracy = r2_score(ytest,lr_pred)
 print(lr_accuracy)
+
+#Decision Tree Regressor
 dt = DecisionTreeRegressor()
 dt.fit(xtrain.values.reshape(-1,1),ytrain)
 dt_pred = dt.predict(xtest.values.reshape(-1,1))
 # print(r2_score(ytest,dt_pred))
 dt_accuracy = r2_score(ytest,dt_pred)
 print(dt_accuracy)
+
+#Rendom Forest Regressor
 rfr = RandomForestRegressor()
 rfr.fit(xtrain.values.reshape(-1,1),ytrain)
 rfr_pred = rfr.predict(xtest.values.reshape(-1,1))
