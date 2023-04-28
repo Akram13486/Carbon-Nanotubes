670b02306eea8c1ee2df97e37a7535b14a17ddce
Akram13486 <114947203+Akram13486@users.noreply.github.com>
2023-04-28 11:18:49 +0530

    Update Carbon NanoTubes.py

--- Carbon NanoTubes.py
+++ Carbon NanoTubes.py
@@ -1,38 +1,38 @@
-import pandas as pd
 import numpy as np
-df=pd.read_csv("breast-cancer.data",header=None,na_values="?")
-for i in range(0,10):
-    print(df[i].unique())
-print(df.isna().sum())
-from sklearn.impute import SimpleImputer
-df_i=pd.DataFrame(SimpleImputer(strategy="most_frequent").fit_transform(df))
-print(df_i.isna().sum())
-from sklearn.preprocessing import LabelEncoder
-for i in range(0,10):
-    df_i[i]=LabelEncoder().fit_transform(df_i[i])
-print(df_i.head())
-target=df_i[0]
-data=df_i.drop(columns=[0])
-print(target.shape,data.shape)
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score,mean_squared_error
+from  sklearn.model_selection import train_test_split
+data=pd.read_csv("carbon_nanotubes_correct.csv")
+print(data.head())
+print(data.isna().sum())
+print(data.info())
+print(data.describe())
+X = data["Calculated atomic coordinates w'"]
+Y = data.drop(columns = "Calculated atomic coordinates w'" )
+xtrain,xtest,ytrain,ytest = train_test_split(X,Y,test_size = 0.3)
+lr  = LinearRegression()
+lr.fit(xtrain.values.reshape(-1,1),ytrain)
+lr_pred = lr.predict(xtest.values.reshape(-1,1))
+lr# print(r2_score(ytest,lr_pred))
+lr_accuracy = r2_score(ytest,lr_pred)
+print(lr_accuracy)
+dt = DecisionTreeRegressor()
+dt.fit(xtrain.values.reshape(-1,1),ytrain)
+dt_pred = dt.predict(xtest.values.reshape(-1,1))
+# print(r2_score(ytest,dt_pred))
+dt_accuracy = r2_score(ytest,dt_pred)
+print(dt_accuracy)
+rfr = RandomForestRegressor()
+rfr.fit(xtrain.values.reshape(-1,1),ytrain)
+rfr_pred = rfr.predict(xtest.values.reshape(-1,1))
 
-from sklearn.model_selection import train_test_split
-x_train,x_test,ytrain,ytest=train_test_split(data,target,test_size=0.3)
-print(x_train.shape,x_test.shape,ytrain.shape,ytest.shape)
+rfr_accuracy = r2_score(ytest,rfr_pred)
+print(rfr_accuracy)
 
-from sklearn.linear_model import Perceptron
-model=Perceptron()
-
-from sklearn.metrics import accuracy_score
-pred_train=model.predict(x_train)
-pred_test=model.predict(x_test)
-print("Accuracy for training ",accuracy_score(pred_train,ytrain))
-print("Accuracy for testing ",accuracy_score(pred_test,ytest))
-
-from sklearn.linear_model import LogisticRegression
-model=LogisticRegression()
-model.fit(x_train,ytrain)
-from sklearn.metrics import accuracy_score
-pred_train=model.predict(x_train)
-pred_test=model.predict(x_test)
-print("Accuracy for training ",accuracy_score(pred_train,ytrain))
-print("Accuracy for testing ",accuracy_score(pred_test,ytest))
+print(mean_squared_error(ytest,lr_pred))
+print(mean_squared_error(ytest,rfr_pred))
+print(mean_squared_error(ytest,dt_pred))
